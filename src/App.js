7030bd843fd47f170d76bf1c0ba09da554b18870
sumit <sumiiitjsahu@gmail.com>
2020-08-09 22:42:47 +0530

    add about section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './component/navbar'
 import Home from './component/home'
 import Header from './component/header'
+import About from './component/about'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <About />
     </div>
     
   );
