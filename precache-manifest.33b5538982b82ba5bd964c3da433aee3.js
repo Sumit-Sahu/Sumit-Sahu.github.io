73fb4bb201892572a1141e4084f4928105b79474
sumit <sumiiitjsahu@gmail.com>
2020-12-18 01:35:50 +0530

    Updates

---
+++ precache-manifest.33b5538982b82ba5bd964c3da433aee3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68231d6687c7688da8570aee3d8d1ccc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a245676f5bafc575704d",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2f520ec85c2f646adbb7",
+    "url": "/static/css/main.a7c26e2f.chunk.css"
+  },
+  {
+    "revision": "a245676f5bafc575704d",
+    "url": "/static/js/2.bf369db6.chunk.js"
+  },
+  {
+    "revision": "6ee87c57e2a7b3f0e0b10d639394c036",
+    "url": "/static/js/2.bf369db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f520ec85c2f646adbb7",
+    "url": "/static/js/main.549baefe.chunk.js"
+  },
+  {
+    "revision": "bdffdc2e3689ff827b51",
+    "url": "/static/js/runtime-main.7bc89341.js"
+  },
+  {
+    "revision": "2558857e7c311aff0a51464f75956565",
+    "url": "/static/media/back.2558857e.jpg"
+  },
+  {
+    "revision": "3b77952c5a76b5cc24054db3328f5092",
+    "url": "/static/media/sumit-resume.3b77952c.pdf"
+  }
+]);
