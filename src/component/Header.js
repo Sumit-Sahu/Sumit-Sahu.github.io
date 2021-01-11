ed2315f7f3c687820c430b024ca03fd5358ba511
Sumit Sahu <sumiiitjsahu@gmail.com>
2021-01-11 23:21:15 +0530

    add social link on home page

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-// import Particles from 'react-particles-js';
+import Particles from 'react-particles-js';
 import TypedText from './Typed'
 
 
@@ -29,19 +29,25 @@ function Header() {
                 /> */}
                 <div className='overlay'>
                     <div className='greet-block'>
-                        <h1 className='greet'>Hi,<br/>
-                            I'm <span className='greet-title_color'>Sumit Sahu</span><br/>
+                        <h1 className='greet'>Hi,<br />
+                            I'm <span className='greet-title_color'>Sumit Sahu</span><br />
                             {/* Software Developer */}
                         </h1>
-                        <div className = 'typed'>
-                        <TypedText 
-                        strings={[
-                            "A CS Undergrad at <span style=color:aquamarine>IIIT Jabalpur</span>",
-                            "I'm a Software Developer",
-                            "Let's Build Together!"
-                            ]}
-                        />
+                        <div className='typed'>
+                            <TypedText
+                                strings={[
+                                    "A CS Undergrad at <span style=color:aquamarine>IIIT Jabalpur</span>",
+                                    "I'm a Software Developer",
+                                    "Let's Build Together!"
+                                ]}
+                            />
                         </div>
+                    </div>
+                    <div className="header_social">
+                        <a href="mailto:sumiiitjsahu@gmail.com"><i className="fas fa-envelope-square" target="_blank" rel="noopener noreferrer"></i></a>
+                        <a href="https://www.linkedin.com/in/sumit-sahu-20b62619b/"><i className="fab fa-linkedin" target="_blank"></i></a>
+                        <a href="https://github.com/Sumit-Sahu"><i className="fab fa-github" target="_blank"></i></a>
+                        <a href="https://stackoverflow.com/users/13489202/sumit"><i className="fab fa-stack-overflow" target="_blank"></i></a>
                     </div>
                 </div>
             </header>
