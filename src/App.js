0d1621223ccf22050b92e945efb91391d4f650ef
Sumit Sahu <sumiiitjsahu@gmail.com>
2021-02-10 23:03:55 +0530

    add footer

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import About from './component/About'
 import Skills from './component/Skills';
 import Projects from './component/Projects';
 import Contact from './component/Contact';
+import Footer from './component/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
       <About />
       <Skills />
       <Projects />
       <Contact />
-    </div>
-    
+      <Footer />
+    </>
+
   );
 }
 
---
+++ src/component/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="footer">
+                <p className="copyright">
+                    &copy; 2021 Sumit Sahu
+                </p>
+                
+            </footer>
+        </>
+    )
+}
+
+export default Footer
